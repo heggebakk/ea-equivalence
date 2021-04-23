ad725bd2c1d5e1ae3b4af649b604102a1ec1cf6d
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-04-23 16:15:19 +0200

    Create basis, find buckets and compare buckets

--- src/functions/permutation.h
+++ src/functions/permutation.h
@@ -1,5 +1,11 @@
 #ifndef SRC_PERMUTATION_H
 #define SRC_PERMUTATION_H
 #include "../utils/structs.h"
-void outerPermutation(struct vbfPartitions partitions)
+/**
+ *
+ * @param f Function F
+ * @param g Function G
+ */
+void outerPermutation(struct vbfPartitions f, struct vbfPartitions g);
+
 #endif //SRC_PERMUTATION_H
--- src/main.c
+++ src/main.c
@@ -1,13 +1,21 @@
 #include "utils/fileParser.h"
 #include "functions/partition.h"
+#include "functions/permutation.h"
 
 typedef struct vbfTruthTable truthTable;
 typedef struct vbfPartitions partitions;
 
 int main(void) {
-    char *filename;
-    filename = "/home/marie/MasterUiB/ea-equivalence/src/resources/TT_library/dim6/q_6_1.tt";
-    truthTable tt = parseTruthTable(filename);
-    partitions partition = partitionFunction(&tt, 4, 0);
+    char *fileF;
+    fileF = "/home/marie/Master-thesis/ea-equivalence/src/resources/TT_library/dim6/q_6_1.tt";
+    truthTable functionF = parseTruthTable(fileF);
+    partitions partitionF = partitionFunction(&functionF, 4, 0);
+    printf("\n");
+    char *fileG;
+    fileG = "/home/marie/Master-thesis/ea-equivalence/src/resources/TT_library/dim6/q_6_2.tt";
+    truthTable functionG = parseTruthTable(fileG);
+    partitions partitionG = partitionFunction(&functionG, 4, 0);
+
+    outerPermutation(partitionF, partitionG);
     return 0;
 }
