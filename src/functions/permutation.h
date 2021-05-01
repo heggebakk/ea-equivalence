f84d4807c0a5519aa3b7951c21adf4192a74f944
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-05-01 15:37:10 +0200

    Fix some memory leaks

--- src/functions/permutation.h
+++ src/functions/permutation.h
@@ -1,5 +1,4 @@
 #ifndef SRC_PERMUTATION_H
 #define SRC_PERMUTATION_H
 #include "../utils/structs.h"
-void outerPermutation(struct vbfPartitions partitions)
 #endif //SRC_PERMUTATION_H
--- src/utils/structs.h
+++ src/utils/structs.h
@@ -1,14 +1,17 @@
 #ifndef SRC_STRUCTS_H
 #define SRC_STRUCTS_H
+#include "stdio.h"
 
 struct vbfTruthTable {
     size_t dimension;
-    size_t elements;
-    int * array;
+    size_t size;
+    int * elements;
 };
 
+void freeTruthTable(struct vbfTruthTable tt);
+
 /**
- * A bucket contains a list of all elements and the size of the bucket.
+ * A bucket contains a list of all size and the size of the bucket.
  */
 struct vbfBucket {
     size_t multiplicity;
@@ -21,6 +24,10 @@ struct vbfPartitions {
     size_t numBuckets;
     struct vbfBucket *buckets;
 };
+
+void freePartition(struct vbfPartitions p);
+
+void freeBuckets(struct vbfBucket *buckets);
 
 struct vbfPermutation {
 
