76548ceaea3aa16f8bd6fd972083ac9ad330c082
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-05-20 14:11:36 +0200

    Add time calculations

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include "utils/fileParser.h"
 #include "functions/partition.h"
 #include "functions/permutation.h"
+#include "time.h"
 
 int main(void) {
+    double timeSpentPartition = 0.0;
+    clock_t startPartition = clock();
+
     char *filename;
     filename = "resources/TT_library/dim6/q_6_1.tt";
     truthTable functionF = parseTruthTable(filename);
@@ -16,13 +20,23 @@ int main(void) {
     partitions partitionG = partitionFunction(&functionG, 4, 0);
     freeTruthTable(functionG);
 
+    clock_t endPartition = clock();
+    timeSpentPartition += (double) (endPartition - startPartition) / CLOCKS_PER_SEC;
+
+    double timeSpentPermutation = 0.0;
+    clock_t startPermutation = clock();
     outerPermutation(partitionF, partitionG);
+    clock_t endPermutation = clock();
+    timeSpentPermutation += (double) (endPermutation - startPermutation) / CLOCKS_PER_SEC;
 
     freeBuckets(&partitionF);
     freeBuckets(&partitionG);
 
     freePartition(partitionF);
     freePartition(partitionG);
+
+    printf("Time spent Partitioning: %f seconds\n", timeSpentPartition);
+    printf("Time spent Permutation: %f seconds\n", timeSpentPermutation);
 
     return 0;
 }
