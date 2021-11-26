7683faef4b771e38668a57d71927e08fa65a0d4c
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-11-26 12:59:08 +0100

    Move createBasis to outerPermutation

--- functions/outerPermutation.c
+++ functions/outerPermutation.c
@@ -28,6 +28,14 @@ void outerPermutation(partitions *f, partitions *g, size_t dimension, size_t *ba
     free(gBucketPosition);
 }
 
+size_t *createBasis(size_t dimension) {
+    size_t *basis = malloc(sizeof(size_t) * dimension + 1);
+    for (size_t i = 0; i < dimension; ++i) {
+        basis[i] = 1L << i;
+    }
+    return basis;
+}
+
 size_t *correspondingBucket(partitions *function, size_t dimension) {
     size_t *list = (size_t *) calloc(sizeof(size_t), 1L << dimension);
     for (size_t i = 0; i < 1L << dimension; ++i) {
