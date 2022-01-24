86e12cdedad0221cfe806f6f01c1d32820e81231
Nikolay <nsk@abc>
2022-01-24 22:04:55 +0100

    Comment recursive().

--- functions/outerPermutation.c
+++ functions/outerPermutation.c
@@ -59,9 +59,16 @@ size_t *correspondingPermutationClass(partitions *partition, size_t dimension) {
     return correspondingClass;
 }
 
+/* This is a DFS for linear permutations respecting the given partitions, i.e. such that every element from a given bucket
+ * with respect to F maps to an element from a bucket with the same size corresponding to G.
+ */
 void recursive(size_t k, const size_t *basis, size_t *images, partitions *partitionF, partitions *partitionG, size_t n,
                size_t *generated, bool *generatedImages, struct ttNode *l1, const size_t *fClassPosition,
                const size_t *gClassPosition) {
+    /* If all basis elements have been assigned an image, and no contradictions have occurred, then we have found
+     * a linear permutation preserving the partitions. We reconstruct its truth-table, and add it to the linked
+     * list containing all permutations found by the search.
+     */
     if (k == n) {
         truthTable * new = malloc(sizeof(truthTable));
         new->dimension = n;
@@ -71,22 +78,45 @@ void recursive(size_t k, const size_t *basis, size_t *images, partitions *partit
         return;
     }
 
+    /* Otherwise, we want to guess the image of basis element number k.
+     * To do this, we first find which bucket with respect to F this 
+     * element belongs.
+     */
     size_t classF = partitionF->classSizes[fClassPosition[basis[k]]];
+    /* We then take the bucket of the same size from the partition with
+     * respect to G. We know that the image of the basis element must
+     * belong to that bucket.
+     */
     size_t posBg = findCorrespondingClass(classF, partitionG);
+    /* We now go through all possible choice from the bucket */
     for (size_t ick = 0; ick < partitionG->classSizes[posBg]; ++ick) {
         size_t ck = partitionG->classes[posBg][ick];
+	/* Since we want the function to be a permutation, the image of the basis element
+	 * should not be one of the images that we have already generated.
+	 */
         if (generatedImages[ck] == true) {
             continue;
         }
+	/* A contradiction can occur if assigning this value to the basis element causes some other
+	 * element to map to the wrong bucket by linearity. The following variable will be set to
+	 * true if such a contradiction is encountered.
+	 */
         bool problem = false;
 
-        /* a very poor but effective way to handle the case when k = 0, because otherwise for k = 0 we get
-         * (1L << (k-1)) == MAX_INT, and it loops figuratively forever */
+	/* This is done to handle the special case of k = 0, since otherwise we get (1L << (k-1)) == MAX_INT. */
         size_t LIMIT = k ? 1L << k : 1;
 
+	/* We now go through all linear combinations of the basis elements that have been previously assigned.
+	 * Adding the newly guessed basis element to such combination allows us to derive one more value of the
+	 * function; if one of these values maps to the wrong bucket, we set "problem" to false to indicate a
+	 * contradiction and backtrack.
+	 */
         for (size_t linearCombination = 0; linearCombination < LIMIT; ++linearCombination) {
             size_t x = linearCombination ^ basis[k];
             size_t y = ck;
+	    /* The following loop simply XOR's all images corresponding to the linear combination, so that
+	     * we get its image by linearity.
+	     */
             if (k) {
                 for (size_t i = 0; i < k; ++i) {
                     if (1L << i & linearCombination) {
@@ -94,19 +124,24 @@ void recursive(size_t k, const size_t *basis, size_t *images, partitions *partit
                     }
                 }
             }
+	    /* Check for contradiction as described above. */
             if (partitionF->classSizes[fClassPosition[x]] != partitionG->classSizes[gClassPosition[y]]) {
                 problem = true;
                 break;
             }
+	    /* Add the new preimage-image pair to the partial truth table of the function */
             generated[x] = y;
+	    /* We also indicate that the image belongs to the set of generated images */
             generatedImages[y] = true;
-        }
+        }* 
+	/* If no contradiction is encountered, we go to the next basis element. */
         if (!problem) {
             images[k] = ck;
             recursive(k + 1, basis, images, partitionF, partitionG, n, generated, generatedImages, l1,
                       fClassPosition, gClassPosition);
         }
 
+	/* When backtracking, we need to reset the generated image indicators */
         for (size_t linearCombinations = 0; linearCombinations < LIMIT; ++linearCombinations) {
             size_t y = ck;
             if (k) {
