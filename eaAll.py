333d76c85f703a69f05ed7cef775398a05e9bcfc
Marie Heggebakk <marie.heggebakk@gmail.com>
2022-03-16 15:45:03 +0100

    Add dimension in first line

---
+++ eaAll.py
@@ -0,0 +1,30 @@
+import time
+import logging
+import os
+import subprocess
+import threading
+
+dim = "dim8/classic"
+dirs = os.listdir(f"./resources/TT_library/{dim}")
+
+
+def permute(name):
+    filename = f"./resources/TT_library/{dim}/{name}"
+    dest_name = f"./results/permutations/{dim}/{name[:-3]}.txt"
+    subprocess.run(["./main.out", '-t', filename, '-f', dest_name, '-o'])
+
+
+if __name__ == '__main__':
+    time_format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=time_format, level=logging.INFO, datefmt="%H:%M:%S")
+    start = time.perf_counter()
+    threads = list()
+    for file in dirs:
+        x = threading.Thread(target=permute, args=(file,))
+        threads.append(x)
+        x.start()
+    for thread in threads:
+        thread.join()
+
+    end = time.perf_counter()
+    print(end-start)
