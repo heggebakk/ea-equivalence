862565e20aacd4888a370ac3b9adee583bed8175
Marie Heggebakk <marie.heggebakk@gmail.com>
2022-03-17 09:31:58 +0100

    Change compile file for groups

--- group.sh
+++ group.sh
@@ -1,2 +1,1 @@
 gcc -lm -o group.out functions/group.c functions/group.h utils/linkedList.h utils/linkedList.c utils/truthTable.h utils/truthTable.c
-#./group.out -t results/permutations/dim6/q_6_3.txt
