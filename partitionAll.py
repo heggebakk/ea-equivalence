10134f024421fba96fd2232fdd260e2b6a22780d
Marie Heggebakk <marie.heggebakk@gmail.com>
2022-03-19 11:34:23 +0100

    Refactor

--- partitionAll.py
+++ partitionAll.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
 
-dirs = os.listdir("./resources/TT_library/dim8/")
+dim = "dim10"
+dirs = os.listdir(f"./resources/TT_library/{dim}/")
 
 for file in dirs:
-    filename = f"./resources/TT_library/dim8/{file}"
-    dest_name = f"./results/partitions/dim8/{file[:-3]}.txt"
+    filename = f"./resources/TT_library/{dim}/{file}"
+    dest_name = f"./results/partitions/{dim}/{file[:-3]}.txt"
     subprocess.run(["./main.out", '-t', filename, '-f', dest_name, '-p'])
